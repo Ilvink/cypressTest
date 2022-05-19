a29675a4742daa7cbe1e0bb4ff7fe5e4377318a8
i.fatkhullina <i.fatkhullina@eot-agregator.ru>
2022-05-20 01:12:04 +0300

    my results with assertions

--- cypress/integration/test myself/tests for first time.js
+++ cypress/integration/test myself/tests for first time.js
@@ -2,5 +2,21 @@
 
 it('first', () => {
 cy.visit('/')
-cy.contains('Каталог')
+cy.contains('Реестр').click()
+cy.get('#filterField-6-dateRange-from')
+  .click()
+  .type('19.05.2022')
+  .should('have.value', '19.05.2022')
+  .and('be.visible')
+cy.contains('Показать').click()
+
 })
+it.only('first', () => {
+  cy.visit('/')
+  cy.contains('Реестр').click()
+  cy.get('#filterField-6-dateRange-from')
+    .click()
+    .type('19.02.2022').then(input=> {
+      expect(input).to.have.value('19.02.2022')
+    })
+  })
