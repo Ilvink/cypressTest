c4715f5c288fd83929b47cc17897001bb84cf0cf
i.fatkhullina <i.fatkhullina@eot-agregator.ru>
2022-05-27 00:31:11 +0300

    all events

--- cypress/integration/test myself/tests for first time.js
+++ cypress/integration/test myself/tests for first time.js
@@ -1,2 +1,39 @@
 /// <reference types="cypress" />
 
+
+it('type', ()=> {
+cy.visit('/')
+cy.contains('Личный кабинет').click()
+.get('.lk-menu-item__title').eq(1).click()
+.get('[placeholder="Логин"]').type('somethink')
+})
+
+it('focus', ()=> {
+    cy.visit('/')
+    cy.contains('Личный кабинет').click()
+    .get('.lk-menu-item__title').eq(1).click()
+    .get('[placeholder="Логин"]')
+    .focus()
+});
+
+it('blur', ()=> {
+        cy.visit('/')
+        cy.contains('Личный кабинет').click()
+        .get('[href="/lk/customer"]').click()   
+    cy.get('#RememberLogin')
+    .check({force: true})
+    .wait(2000)
+    .uncheck({force: true})        
+});
+
+it('blur', ()=> {
+cy.visit('/')
+.get('[href="/feedback"]').eq(1)
+.wait(2000)
+.scrollIntoView()       
+});
+
+it.only('blur', ()=> {
+    cy.visit('/')
+    cy.scrollTo(0,550)      
+});
