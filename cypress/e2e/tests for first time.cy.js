3a531b29a7a652b66a5f6b7a721c187e35ff89f0
Ilvink <ilvinkaFat@mail.ru>
2022-06-15 22:54:35 +0300

    blah

--- cypress/e2e/tests for first time.cy.js
+++ cypress/e2e/tests for first time.cy.js
@@ -77,8 +77,89 @@ it('API test', () =>{
   }).then((response) => {
       expect(response).to.have.property('status').to.equal(200)
       expect(response.body).to.have.property('totalCount').to.equal(1)
-      expect('[Array(1)]').to.have.property('tradeNumber').to.equal("111111100122102287")
+      expect(response.body.items[0]).to.have.property('tradeNumber').to.equal("111111100122102287")
       console.log(response.body)
       
     });
+});
+it.only('API test with Should', () =>{
+    const reqBody ={
+        "page": 1,
+        "size": 10,
+        "isReviewAwaiting": false,
+        "isCustomerSendingAwaiting": false,
+        "isCustomerSigningAwaiting": false,
+        "isSupplierSigningAwaiting": false,
+        "isChangeDealTermsProtocolReceived": false,
+        "isWinner": false,
+        "isLoser": false,
+        "searchText": "",
+        "purchaseName": "",
+        "number": "111111100122102287",
+        "lotItemEatCodes": [],
+        "productCode": null,
+        "okpd2Codes": [],
+        "ktruCodes": [],
+        "purchaseTypeIds": [],
+        "types": [],
+        "customerId": null,
+        "customerNameOrInn": null,
+        "customerInn": null,
+        "customerKpp": null,
+        "supplierNameOrInn": null,
+        "purchaseMethods": [
+            4
+        ],
+        "priceStart": null,
+        "priceEnd": null,
+        "deliveryAddressRegionCodes": [],
+        "deliveryAddress": null,
+        "contractPriceStart": null,
+        "contractPriceEnd": null,
+        "applicationFillingStartDate": null,
+        "applicationFillingEndDate": null,
+        "contractSignDateStart": null,
+        "contractSignDateEnd": null,
+        "deliveryDateStart": null,
+        "deliveryDateEnd": null,
+        "isSmpOnly": false,
+        "isEatOnly": false,
+        "stateDefenseOrderOnly": null,
+        "createDateTime": null,
+        "excludeCancelledByCustomer": false,
+        "excludeExternalTrades": false,
+        "publishDateBegin": null,
+        "publishDateEnd": null,
+        "updateDateBegin": null,
+        "updateDateEnd": null,
+        "applicationFillingStartDateBegin": null,
+        "applicationFillingStartDateEnd": null,
+        "customerContractNumber": null,
+        "hasLinkedExternalTrade": null,
+        "eisTradeNumber": null,
+        "isSpecificSupplier": false,
+        "isRussianItemsPurchase": null,
+        "organizerRegion": null,
+        "lotStates": [],
+        "sort": [
+            {
+                "fieldName": "publishDate",
+                "direction": 2
+            }
+        ]
+    }
+    const Headers = {
+        authority:"tender-cache-api.rc.eot-agregator.ru"
+    }
+  cy.request({
+      method: "POST",
+      url: "https://tender-cache-api.rc.eot-agregator.ru/api/TradeLot/list-published-trade-lots",
+      body: reqBody,
+      headers: Headers
+  }).its('body').should('contain',{
+    isFail: false
+  }).its('items[0]').should('contain',{
+    tradeNumber: "111111100122102287"
+
+})
 });
